be3ce03f619c809dcb7e114d1c62dbc2c08f8bb8
Nick Lorch <nlorch@gmail.com>
2020-05-22 09:54:35 -0500

    wordlist v0.2.1

---
+++ wordlist/precache-manifest.f8e0d86ca89055f998390aaead0b718d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b3c504903c907af551e3",
+    "url": "/wordlist/static/js/runtime~main.b3c50490.js"
+  },
+  {
+    "revision": "4861e96f2fff66370e7a",
+    "url": "/wordlist/static/js/main.4861e96f.chunk.js"
+  },
+  {
+    "revision": "4d91f67a58526f5fe88d",
+    "url": "/wordlist/static/js/1.4d91f67a.chunk.js"
+  },
+  {
+    "revision": "4861e96f2fff66370e7a",
+    "url": "/wordlist/static/css/main.bfec3ee2.chunk.css"
+  },
+  {
+    "revision": "4d91f67a58526f5fe88d",
+    "url": "/wordlist/static/css/1.f69d9e24.chunk.css"
+  },
+  {
+    "revision": "f9b6c779ecdf66630ce83e694a084905",
+    "url": "/wordlist/index.html"
+  }
+];
